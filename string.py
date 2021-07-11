8846ffc14e0dd385c163eee29673f74a3851961f
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-12 00:22:04 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,11 @@
+x = "Hello World"
+txt = " Hello World "
+age = 36
+Txt = "My name is John, and I am {}"
+print(len(x))                           #return the length of the string
+print(x[0])                             #return the first character of the string txt.
+print (x[2:5])                          #return the characters from index 2 to index 4
+print(txt.strip())                      #Return the string without any whitespace at the beginning or the end.
+print(txt.upper())                      #Convert the value of txt to upper case.
+print(txt.lower())                      #convert the value of txt to lower case.
+print(Txt.format(age))                  #syntax to add a placeholder for the age parameter.
