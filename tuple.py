b6ecff29774eb3c554cc54914f4c4e54d8824d9e
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-12 14:11:42 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,5 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])                                  #to print the first item in the fruits tuple.
+print(len(fruits))                                #to print the number of items in the fruits tuple.
+print(fruits[-1])                                 #negative indexing to print the last item in the tuple.
+print(fruits[2:5])                                #range of indexes to print the third, fourth, and fifth item in the tuple.
