dcf7f618fc5cae64813a93e72b924b02c48d6b75
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-12 16:03:28 +0530

    Update cnd.py

--- cnd.py
+++ cnd.py
@@ -2,15 +2,15 @@ a = 50
 b = 10
 c = 40
 d = 20
-if a > b:
+if a > b:                                              # Print "Hello World" if a is greater than b.
+   print("Hello World")                
+if c!=d:                                               #Print "Hello World" if a is not equal to b.
    print("Hello World")
-if c!=d:
-   print("Hello World")
-if a == b:
+if a == b:                                             #Print "Yes" if a is equal to b, otherwise print "No".
    print("Yes")
 else:
    print("No")
-if a == b:
+if a == b:                                             #Print "1" if a is equal to b, print "2" if a is greater than b, otherwise print "3".
     print("1")
 elif a > b:
     print("2")
