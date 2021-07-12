5470da627f3692c875abf1270c5ee01d9aedb11e
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-12 15:59:53 +0530

    Add files via upload
    conditionals

---
+++ cnd.py
@@ -0,0 +1,18 @@
+a = 50
+b = 10
+c = 40
+d = 20
+if a > b:
+   print("Hello World")
+if c!=d:
+   print("Hello World")
+if a == b:
+   print("Yes")
+else:
+   print("No")
+if a == b:
+    print("1")
+elif a > b:
+    print("2")
+else:
+    print("3")
