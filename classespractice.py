5338dda0ca97f6377a9169b81fa54b5bc8e6eeb8
Sanchita Sharma <65801512+sanchitasharma2001@users.noreply.github.com>
2021-07-26 21:20:48 +0530

    Create classespractice.py

---
+++ classespractice.py
@@ -0,0 +1,43 @@
+#Create a class named MyClass:
+class
+ MyClass:
+  x = 5
+  
+  #Create an object of MyClass called p1:
+class MyClass:
+  x = 5
+p1 = MyClass()
+  
+#Use the p1 object to print the value of x:
+class MyClass:
+  x = 5
+p1 = MyClass()
+print(p1.x)  
+  
+#What is the correct syntax to assign a "init" function to a class?
+class Person:
+  def 
+__init__
+(self, name, age):
+    self.name = name
+    self.age = age  
+    
+#What is the correct syntax to create a class named Student that will inherit properties and methods from a class named Person?
+class 
+Student(Person):
+  
+#We have used the Student class to create an object named x.
+#What is the correct syntax to execute the printname method of the object x?
+class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
+  
